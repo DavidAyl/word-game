a1d217036ab37380719c9a26e3681bc5bc4ff03a
David Aylward <Davidayl14@outlook.com>
2022-02-28 14:35:31 -0500

    finished js

--- main/script.js
+++ main/script.js
@@ -1,5 +1,32 @@
-console.log("hello");
+var gameWordElement = document.getElementById("gameWord");
+var gameWord = "javascript";
+var underScores = [];
+
+function getUnderscores() {
+  for (let i = 0; i < gameWord.length; i++) {
+    underScores.push("_ ");
+  }
+}
+
+function displayWord() {
+  gameWordElement.innerHTML = underScores.join("");
+}
+function checkWin() {
+  console.log("we r in the check win function!!!");
+  if (gameWord === underScores.join("")) {
+    alert("YOU WON!!!!!");
+  }
+}
+
+getUnderscores();
+displayWord();
 
 document.addEventListener("keydown", function (event) {
-  console.log("Key Got Pressed", event.key);
+  for (let i = 0; i < gameWord.length; i++) {
+    if (gameWord[i] === event.key) {
+      underScores[i] = event.key;
+    }
+  }
+  checkWin();
+  displayWord();
 });
